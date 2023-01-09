d75614e8c4ba97c38c3429e38a0e33b0d9af0381
Sharun <101327895+SHARUN266@users.noreply.github.com>
2023-01-09 17:03:26 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const  mongoose  = require("mongoose")
 
-mongoose.connect('mongodb+srv://sharun:123@atlascluster.qwa1fxi.mongodb.net/AuthRest?retryWrites=true&w=majority',()=>{
+mongoose.connect('MongoDbURLComeHere...',()=>{
     console.log(`Hey! Your mongoDB connected`)
 })
 
